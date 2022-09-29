8fb7a70a398f0a17cfac4e52d64b15bb4b29273b
leela khadka <khadkaleela3@gmail.com>
2022-09-28 22:06:07 -0700

    completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name) 
+      self.freebies.find_by(item_name: item_name)
+    end
+
+    def give_away(dev, freebie) 
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+            freebie.save
+        else
+            "You don't own that freebie"
+        end
+    end
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,16 @@
 class Freebie < ActiveRecord::Base
+    
+    belongs_to :company
+    belongs_to :dev
+
+
+    def print_details
+     dev_name = self.dev.name
+     company_name = self.company.name
+     item_name = self.item_name
+     
+     "#{dev_name} owns a #{item_name} from #{company_name}"
+    end
+
 
 end
